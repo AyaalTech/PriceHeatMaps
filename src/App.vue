bd46e50998f8112e3bb9e824b0f9634b7fa042f4
Evgenii Gerasimov <103824860+SawMassacre@users.noreply.github.com>
2023-08-21 18:58:45 +1000

    ✨ feat(App.vue): OL heatmap implemented
    Done implementing demo, cities look kind of shrinked though

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ol-map
+    ref="map"
+    :loadTilesWhileAnimating="true"
+    :loadTilesWhileInteracting="true"
+    style="height: 100vh"
+  >
+    <ol-view
+      ref="view"
+      :center="center"
+      :rotation="rotation"
+      :zoom="zoom"
+      :projection="projection"
+    />
+
+    <ol-tile-layer>
+      <ol-source-osm />
+    </ol-tile-layer>
+
+    <ol-heatmap-layer
+      title="heatmap"
+      :blur="20"
+      :radius="20"
+      :weight="heatmapWeight"
+      :zIndex="1"
+    >
+      <ol-source-vector
+        ref="earthquakes"
+        url="https://raw.githubusercontent.com/openlayers/openlayers/main/examples/data/kml/2012_Earthquakes_Mag5.kml"
+        :format="kml"
+      >
+      </ol-source-vector>
+    </ol-heatmap-layer>
+  </ol-map>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref, inject } from "vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const center = ref([101.97, 4.21]);
+const projection = ref("EPSG:4326");
+const zoom = ref(5);
+const rotation = ref(0);
+const format = inject("ol-format");
+const kml = new format.KML({ extractStyles: false });
+const heatmapWeight = function (feature) {
+  // 2012_Earthquakes_Mag5.kml stores the magnitude of each earthquake in a
+  // standards-violating <magnitude> tag in each Placemark.  We extract it from
+  // the Placemark's name instead.
+  const name = feature.get("name");
+  const magnitude = parseFloat(name.substr(2));
+  return magnitude - 5;
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.overlay-content {
+  background: red !important;
+  color: white;
+  box-shadow: 0 5px 10px rgb(2 2 2 / 20%);
+  padding: 10px 20px;
+  font-size: 16px;
+}
+
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import OpenLayersMap from "vue3-openlayers";
+import "vue3-openlayers/styles.css"; // vue3-openlayers version < 1.0.0-*
+
+const app = createApp(App);
+app.use(OpenLayersMap /* options */);
+
+app.mount("#app");
