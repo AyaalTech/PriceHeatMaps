fbe9d42aa22741dd76b64dfaa4331d175de9af92
Evgenii Gerasimov <103824860+SawMassacre@users.noreply.github.com>
2023-08-23 02:11:10 +1000

    ✨ feat: Basic heatmap implemented
    Done creating basic heatmap, got to style it though

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,11 @@
       :weight="heatmapWeight"
       :zIndex="10"
     >
-      <ol-source-vector :features="geojsonData" :format="geojson">
+      <ol-source-vector
+        ref="priceheatmap"
+        url="https://raw.githubusercontent.com/SawMassacre/PriceHeatMaps/131bbdf9c87f275d5745f8e4e62179cc9af64545/src/data.geojson"
+        :format="geojson"
+      >
       </ol-source-vector>
     </ol-heatmap-layer>
   </ol-map>
@@ -39,18 +43,19 @@ import FullscreenControl from "./components/FullscreenControl.vue";
 import { ref, inject } from "vue";
 import { fromLonLat } from "ol/proj";
 
-const format = inject("ol-format");
-const geojson = new format.GeoJSON({ extractStyles: false });
 const center = ref(fromLonLat([49.12, 55.78]));
 const projection = ref("EPSG:3857");
 const zoom = ref(13);
 const rotation = ref(0);
 
+const format = inject("ol-format");
+const geojson = new format.GeoJSON({ extractStyles: false });
+
 const heatmapWeight = function (feature) {
   const properties = feature.getProperties();
   const price = parseFloat(properties.price);
 
-  const scaleFactor = 0.5;
+  const scaleFactor = 0.000000002;
 
   return price * scaleFactor;
 };
