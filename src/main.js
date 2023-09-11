83f1a58897ad4fb3b59c7666ce8664129a75ba9e
Evgenii Gerasimov <103824860+SawMassacre@users.noreply.github.com>
2023-09-11 20:33:36 +1000

    🦄 refactor: Project refactoring
    Just copied other project into git folder

--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-import OpenLayersMap from "vue3-openlayers";
-import "vue3-openlayers/styles.css"; // vue3-openlayers version < 1.0.0-*
-
-const app = createApp(App);
-app.use(OpenLayersMap /* options */);
-
-app.mount("#app");
+createApp(App).mount('#app')
